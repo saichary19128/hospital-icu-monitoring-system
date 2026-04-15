b050a177e2129d948a8d7b1feb793a545de6eff2
vadla sai kumar <148974392+saichary19128@users.noreply.github.com>
2026-04-15 23:30:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.use("/api/beds", bedRoutes);
 app.get("/", (req, res) => {
   res.send("API Running 🚀");
 });
-const PORT=process.env.PORT || 50000;
+const PORT=process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
